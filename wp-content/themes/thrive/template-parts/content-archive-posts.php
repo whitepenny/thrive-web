960f3e3293614f2cdd92de934f5e48383b6d8e13
Scott Revell <srevell@whitepenny.com>
2016-01-07 10:08:30 -0500

    Moved Files to appropriate location

--- wp-content/themes/thrive/template-parts/content-archive-posts.php
+++ wp-content/themes/thrive/template-parts/content-archive-posts.php
@@ -12,16 +12,15 @@
 
 		<div class="entry-meta">
 		    <?php thrive_posted_on_single(); ?>
-                    
-                    <div class="single-cats">
-                        <?php thrive_entry_footer(); ?>
-                    </div>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
                 <h2 class="entry-title"><a href="<?php echo the_permalink() ?>"><?php the_title(); ?></a></h2>
 		<?php the_excerpt(); ?>
+                <div class="single-cats">
+                        <span class="filed-under">Filed Under: </span><?php thrive_entry_footer(); ?>
+                    </div>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'thrive' ),
@@ -32,4 +31,3 @@
 
 	
 </article><!-- #post-## -->
-
--- wp-content/themes/thrive/template-parts/content-single.php
+++ wp-content/themes/thrive/template-parts/content-single.php
@@ -13,13 +13,19 @@
 		<div class="entry-meta">
 			<?php thrive_posted_on_single(); ?><span class="sep"> | </span><?php thrive_entry_footer(); ?>
 		</div><!-- .entry-meta -->
+                <div class="single-post-social-top">
+                     <?php echo sharing_display(); ?>
+                </div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
             <div class="featured-img">
                 <?php if ( has_post_thumbnail() ) {
-			the_post_thumbnail('blog-size');
+			/*the_post_thumbnail('blog-size');*/
+                        the_post_thumbnail('full');
 		}  ?> 
+            </div>
+            <div class="posted-on"><span>Posted by <?php the_field('who_wrote_it'); ?></span>
             </div>
 		<?php the_content(); ?>
 		
