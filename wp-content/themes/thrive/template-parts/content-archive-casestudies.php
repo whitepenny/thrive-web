bf1459d862fbcd2a5ea32cde2751e1f8794ee799
Scott Revell <srevell@whitepenny.com>
2016-10-14 16:02:29 -0400

    Case Studies layout

--- wp-content/themes/thrive/template-parts/content-archive-casestudies.php
+++ wp-content/themes/thrive/template-parts/content-archive-casestudies.php
@@ -1,15 +1,40 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<a href="<?php echo the_permalink(); ?>">
-		<div class="entry-content">
+<?php if( get_field('link_from_archive_page') )
+    {
+?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('csa-linked-article'); ?>>
+        <a href="<?php echo the_permalink(); ?>">
+            <div class="entry-content">
            
-               		<div class="csa-featured-img">
-                   	<?php if ( has_post_thumbnail() ) {
-		       		the_post_thumbnail('blog-size');
-	           	}  ?>
+                <div class="csa-featured-img">
+                    <?php if ( has_post_thumbnail() ) {
+                        the_post_thumbnail('blog-size');
+                    }  ?>
                    
-               		</div>
-	    
-		</div><!-- .entry-content -->
-	</a> 
-</article><!-- #post-## -->
+                </div>
+      
+            </div><!-- .entry-content -->
+        </a> 
+        <span class="csa-readmore cat-links"><a href="<?php the_permalink(); ?>">Read Success Story</a></span>
+    </article><!-- #post-## -->
 
+<?php }
+ else
+    {
+?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <div class="csa-bounding-box">
+          <div class="entry-content">
+               
+              <div class="csa-featured-img">
+                  <?php if ( has_post_thumbnail() ) {
+                  the_post_thumbnail('blog-size');
+                  }  ?>
+                       
+              </div>
+          
+          </div><!-- .entry-content -->
+      </div> 
+    </article><!-- #post-## -->
+<?php
+}
+?>
